426866c9c9b9dffa6925621449e87b7a62435289
kronos-optimize <phalla.chum@student.cadt.edu.kh>
2025-03-21 15:35:14 +0700

    Initial commit

---
+++ search_file.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#prompts user for directory path
+read -p "Enter directory path to search for a file: " dir_path
+dir_path=$(eval echo "$dir_path")
+
+#prompts user for file name or extension to search for
+read -p "Enter the file name or extension to search for(e.g. *.txt): " file_ext
+
+#check if directory exists
+if [ -d "$dir_path" ]; then
+	#search for file in directory
+	find "$dir_path" -type f -name "$file_ext"
+	echo "$(date): Successfully searched for '$file_ext' in '$dir_path'" >> script.log
+else
+	echo "Invalid directory path!"
+	echo "$(date): Failed to searched for '$file_ext' in '$dir_path'" >> script.log
+fi
