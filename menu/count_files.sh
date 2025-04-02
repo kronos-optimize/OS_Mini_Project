cd70bf02192029ae9b5741b911dfc9d6a04fc19b
Phalla Chum <phalla.chum@student.cadt.edu.kh>
2025-04-02 22:43:53 +0700

    Update count_files.sh

--- menu/count_files.sh
+++ menu/count_files.sh
@@ -13,8 +13,8 @@ dir=$(eval echo "$dir")
 if [ -d "$dir" ]; then
     count=$(find "$dir" -type f | wc -l)
     echo "Counting number of files in '$dir': $count"
-    echo "$(date): Counted files in '$dir': $count files." >> script.log
+    echo "$(date): Counted files in '$dir': $count files." >> ../script.log
 else
     echo "Invalid directory."
-    echo "$(date): Failed to count files in '$dir'." >> script.log
+    echo "$(date): Failed to count files in '$dir'." >> ../script.log
 fi
