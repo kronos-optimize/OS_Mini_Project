bae347738d46939c496392a97c180f329af2530a
Phalla Chum <phalla.chum@student.cadt.edu.kh>
2025-04-02 22:45:01 +0700

    Update list_files.sh

--- menu/list_files.sh
+++ menu/list_files.sh
@@ -15,21 +15,21 @@ if [ -d "$dir_path" ]; then
     # Check if directory is empty
     if [ -z "$(ls -A "$dir_path")" ]; then
         echo "The directory '$dir_path' is empty."
-        echo "$(date): The directory '$dir_path' is empty." >> script.log
+        echo "$(date): The directory '$dir_path' is empty." >> ../script.log
     else
         ls -l "$dir_path"  # Lists all files with detailed info
 
         # Check if the ls command was successful
         if [ $? -eq 0 ]; then
             # Log action on success
-            echo "$(date): Listed files in directory '$dir_path'." >> script.log
+            echo "$(date): Listed files in directory '$dir_path'." >> ../script.log
         else
             # Log action on failure
-            echo "$(date): Failed to list files in '$dir_path'." >> script.log
+            echo "$(date): Failed to list files in '$dir_path'." >> ../script.log
         fi
     fi
 else
     # Invalid directory
     echo "Invalid directory path!"
-    echo "$(date): Failed to list files in '$dir_path'" >> script.log
+    echo "$(date): Failed to list files in '$dir_path'" >> ../script.log
 fi
